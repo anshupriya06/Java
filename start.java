301ae083b317fb02bdade5480a9aec22bb976edb
Anshu Priya <anshupriyadnr06@gmail.com>
2024-09-15 10:41:15 +0530

    added inputs

---
+++ start.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class start{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("You entered: " + n);        
+        System.out.println("Hello chikki");
+    }
+}
